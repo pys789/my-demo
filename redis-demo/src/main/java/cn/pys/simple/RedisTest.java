0b02426dd7b2cebc0a472c4147739fa89046f12a
2998 <pengys@yunjiglobal.com>
2020-12-03 18:17:47 +0800

    添加redis执行lua脚本

--- redis-demo/src/main/java/cn/pys/simple/RedisTest.java
+++ redis-demo/src/main/java/cn/pys/simple/RedisTest.java
@@ -2,6 +2,8 @@ package cn.pys.simple;
 
 import redis.clients.jedis.Jedis;
 
+import java.util.Arrays;
+
 /**
  * @Description
  * @Date 2020/11/18 17:36
@@ -13,6 +15,12 @@ public class RedisTest {
         Jedis jedis = new Jedis("localhost", 6379);
         //jedis.auth("");
         jedis.set("name2","pys2");
+        //String load = "return redis.call('set',KEYS[1],ARGV[1])";
+//c686f316aaf1eb01d5a4de1b0b63cd233010e63d
+        //String load = jedis.scriptLoad("return redis.call('set',KEYS[1],ARGV[1])");
+        //Object test = jedis.eval(load, Arrays.asList("a"), Arrays.asList("1"));
+        Object test = jedis.evalsha("c686f316aaf1eb01d5a4de1b0b63cd233010e63d", Arrays.asList("b"), Arrays.asList("2"));
+        System.out.println(test);
         String name = jedis.get("name2");
         System.out.println(name);
     }
