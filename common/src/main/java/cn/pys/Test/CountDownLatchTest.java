a4ca3dd6d87a1f8848dfa28e4e6be4a5545cb9a6
2998 <pengys@yunjiglobal.com>
2020-12-24 14:39:20 +0800

    提交CountDownLatch和CycleBarrier示例

---
+++ common/src/main/java/cn/pys/Test/CountDownLatchTest.java
@@ -0,0 +1,35 @@
+package cn.pys.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountDownLatchTest {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            new Thread(new readNum(i, countDownLatch)).start();
+        }
+        // countDownLatch做减法，调用countDown时减一，达到0时执行主线程await后面的代码
+        countDownLatch.await();
+        // 等其他线程执行完再执行主线程
+        System.out.println("主线程组执行结束");
+    }
+
+    static class readNum implements Runnable {
+        private int id;
+        private CountDownLatch latch;
+
+        readNum(int id, CountDownLatch latch) {
+            this.id = id;
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                System.out.println("id:" + id);
+                latch.countDown();
+                System.out.println("线程组任务" + id + "结束，其他任务继续");
+            }
+        }
+    }
+}
---
+++ common/src/main/java/cn/pys/Test/CyclicBarrierTest.java
@@ -0,0 +1,58 @@
+package cn.pys.Test;
+
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest {
+    public static void main(String[] args) throws InterruptedException {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(5, new Runnable() {
+            // Barrier 做加法，await达到5次时执行下面的方法，其他线程继续执行
+            @Override
+            public void run() {
+                System.out.println("线程组执行结束");
+            }
+        });
+        for (int i = 0; i < 5; i++) {
+            new Thread(new readNum(i, cyclicBarrier)).start();
+        }
+        // 不影响主线程的执行
+        System.out.println("主线程组执行结束");
+
+        //CyclicBarrier 可以重复利用，这个是CountDownLatch做不到的
+        /*for (int i = 10; i < 15; i++) {
+            new Thread(new readNum(i, cyclicBarrier)).start();
+        }*/
+    }
+
+    static class readNum implements Runnable {
+        Random random = new Random();
+        private int id;
+        private CyclicBarrier cyc;
+
+        readNum(int id, CyclicBarrier cyc) {
+            this.id = id;
+            this.cyc = cyc;
+        }
+
+        @Override
+        public void run() {
+            synchronized (this) {
+                System.out.println("id:" + id);
+                int randomNum = random.nextInt(2000) + 1000;
+                try {
+                    cyc.await();
+                    Thread.sleep(randomNum);
+                    System.out.println("线程组任务1:" + id + "结束，其他任务继续");
+                    cyc.await();
+                    Thread.sleep(randomNum);
+                    System.out.println("线程组任务2:" + id + "结束，其他任务继续");
+                    cyc.await();
+                    Thread.sleep(randomNum);
+                    System.out.println("线程组任务3:" + id + "结束，其他任务继续");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
